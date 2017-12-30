3b496cca9eb6c71655e05022ed43ed05f958ae95
Lee il-kyu <dlfrb2002@gmail.com>
2017-12-30 18:48:03 +0900

    20171230 commit(libftp commit)

--- c/libftp/libftp.c
+++ c/libftp/libftp.c
@@ -7,8 +7,6 @@ int main()
     ftpFileInfo_t ftpFileInfo;
     char* fileBuffer = NULL;
 
-    
-    
     /* memset */
     memset(&ftpConnectInfo,0x00,sizeof(ftpConnectInfo_t));
     memset(&ftpFileInfo,0x00,sizeof(ftpFileInfo_t));
@@ -23,7 +21,6 @@ int main()
     {
         printf("getFtpFileInfo() ERROR!!  \n");
         return ERROR;
-
     }
 
     if(ftpFileGet(&ftpConnectInfo,&ftpFileInfo) == ERROR)
@@ -32,25 +29,41 @@ int main()
         return ERROR;
     }
 
+    if(ftpFilePut(&ftpConnectInfo,&ftpFileInfo) == ERROR)
+    {
+        printf("ftpFilePut() ERROR!!  \n");
+        return ERROR;
+    }
+
     //fileBuffer = malloc(sizeof(char) * ftpFileInfo.fileSize+1);
 
+#if 0
     if(fileReadData(&ftpFileInfo,&fileBuffer) == ERROR)
     {
         printf("fileReadData() ERROR!!  \n");
         return ERROR;
         
     }
+#endif
 
+#if 0
     printf("fileData : %s",fileBuffer);
+#endif
 
     return SUCCESS;
 }
 
 int getConnectInfo(ftpConnectInfo_t* ftpConnectInfo)
 {
-    strcpy(ftpConnectInfo->ip,"11111");
-    strcpy(ftpConnectInfo->loginID,"iiii");
-    strcpy(ftpConnectInfo->loginPassword,"ppppp");
+    /*Get Ftp Server Info */
+    strcpy(ftpConnectInfo->getFtpIp,GET_FTP_IP);
+    strcpy(ftpConnectInfo->getFtpLoginId,GET_FTP_LOGIN_ID);
+    strcpy(ftpConnectInfo->getFtpLoginPassword,GET_FTP_LOGIN_PSWD);
+
+     /*Put Ftp Server Info */
+    strcpy(ftpConnectInfo->putFtpIp,PUT_FTP_IP);
+    strcpy(ftpConnectInfo->putFtpLoginId,PUT_FTP_LOGIN_ID);
+    strcpy(ftpConnectInfo->putFtpLoginPassword,PUT_FTP_LOGIN_PSWD);
 
     return SUCCESS;
 }
@@ -58,11 +71,17 @@ int getConnectInfo(ftpConnectInfo_t* ftpConnectInfo)
 
 int getFtpFileInfo(ftpFileInfo_t* ftpFileInfo)
 {
-    strcpy(ftpFileInfo->srcFilePath,"aaaa");
-    strcpy(ftpFileInfo->srcFileName,"bbbb");
+    /* getFtp */
+    strcpy(ftpFileInfo->getFtpSrcFilePath,GET_FTP_SRC_FILEPATH);
+    strcpy(ftpFileInfo->getFtpSrcFileName,GET_FTP_SRC_FILENAME);
+    strcpy(ftpFileInfo->getFtpDestFilePath,GET_FTP_DEST_FILEPATH);
+    strcpy(ftpFileInfo->getFtpDestFileName,GET_FTP_DEST_FILENAME);
 
-    strcpy(ftpFileInfo->destFilePath,"ddddd/");
-    strcpy(ftpFileInfo->destFileName,"get.dat");
+    /*putFtp */
+    strcpy(ftpFileInfo->putFtpSrcFilePath,PUT_FTP_SRC_FILEPATH);
+    strcpy(ftpFileInfo->putFtpSrcFileName,PUT_FTP_SRC_FILENAME);
+    strcpy(ftpFileInfo->putFtpDestFilePath,PUT_FTP_DEST_FILEPATH);
+    strcpy(ftpFileInfo->putFtpDestFileName,PUT_FTP_DEST_FILENAME);
 
     return SUCCESS;
 }
@@ -73,56 +92,97 @@ int ftpFileGet(ftpConnectInfo_t* ftpConnectInfo, ftpFileInfo_t* ftpFileInfo)
     char dest[100+1];
 
     netbuf* netBuf = NULL;
-    //netbuf* dataBuf = NULL;
 
-    memset(src,0x00,sizeof(src));
     memset(dest,0x00,sizeof(dest));
-
-
+    memset(src,0x00,sizeof(src));
+   
     /* init libftp */
     FtpInit();
 
-    if(FtpConnect(ftpConnectInfo->ip,&netBuf) != 1)
+    if(FtpConnect(ftpConnectInfo->getFtpIp,&netBuf) != 1)
     {
         printf("FtpConnect() Connection Failed!!! \n");
         return ERROR;
     }
 
-    if(FtpLogin(ftpConnectInfo->loginID,ftpConnectInfo->loginPassword,netBuf) != 1)
+    if(FtpLogin(ftpConnectInfo->getFtpLoginId,ftpConnectInfo->getFtpLoginPassword,netBuf) != 1)
     {
-        printf("FtpLogin() Connection Failed!!! \n");
+        FtpQuit(netBuf);
+        printf("FtpLogin() Failed!!! \n");
         return ERROR;
     }
 
-     strcat(src,ftpFileInfo->srcFilePath);
-     strcat(src,ftpFileInfo->srcFileName);
+    sprintf(dest,"%s/%s",ftpFileInfo->getFtpDestFilePath, ftpFileInfo->getFtpDestFileName);
+    sprintf(src,"%s/%s",ftpFileInfo->getFtpSrcFilePath, ftpFileInfo->getFtpSrcFileName);
 
-     strcat(dest,ftpFileInfo->destFilePath);
-     strcat(dest,ftpFileInfo->destFileName);
+     printf("ftpFileGet() src : %s \n",src);
+     printf("ftpFileGet() dest : %s \n",dest);
 
-     printf("src : %s \n",src);
-     printf("dest : %s \n",dest);
-
-    if(FtpSize(src,(&ftpFileInfo->fileSize),FTPLIB_IMAGE,netBuf) != 1)
+    if(FtpSize(dest,(&ftpFileInfo->fileSize),FTPLIB_IMAGE,netBuf) != 1)
     {
-        printf("FtpSize() Connection Failed!!! \n");
+        FtpQuit(netBuf);
+        printf("FtpSize() Failed!!! \n");
         return ERROR;
     }
 
-    printf("fileSize : %d \n",ftpFileInfo->fileSize);
+    printf("ftpGet() fileSize : %d \n",ftpFileInfo->fileSize);
     
-     if(FtpGet(dest,src,FTPLIB_IMAGE,netBuf) != 1 )
+     if(FtpGet(src,dest,FTPLIB_IMAGE,netBuf) != 1 )
      {
+         FtpQuit(netBuf);
          printf("FtpGet() ERROR !!!!  \n");
          return ERROR;
      }
 
-     
+     FtpClose(netBuf);
 
     return SUCCESS;
 }
 
 
+int ftpFilePut(ftpConnectInfo_t* ftpConnectInfo, ftpFileInfo_t* ftpFileInfo)
+{
+    char src[100+1];
+    char dest[100+1];
+
+    netbuf* netBuf = NULL;
+
+    memset(dest,0x00,sizeof(dest));
+    memset(src,0x00,sizeof(src));
+
+    FtpInit();
+
+    if(FtpConnect(ftpConnectInfo->putFtpIp,&netBuf) != 1)
+    {
+        printf("FtpConnect() Connection Failed!!! \n");
+        return ERROR;
+    }
+
+    if(FtpLogin(ftpConnectInfo->putFtpLoginId,ftpConnectInfo->putFtpLoginPassword,netBuf) != 1)
+    {
+        printf("FtpLogin() Failed!!! \n");
+        return ERROR;
+    }
+
+    sprintf(dest,"%s/%s",ftpFileInfo->putFtpDestFilePath, ftpFileInfo->putFtpDestFileName);
+    sprintf(src,"%s/%s",ftpFileInfo->putFtpSrcFilePath, ftpFileInfo->putFtpSrcFileName);
+
+    printf("ftpFilePut src : %s \n",src);
+    printf("ftpFilePut dest : %s \n",dest);
+
+    if(FtpPut(src,dest,FTPLIB_IMAGE, netBuf) != 1)
+    {
+        printf("FtpPut() Failed!!! \n");
+        return ERROR;
+    }
+
+    FtpClose(netBuf);
+
+    return SUCCESS;
+}
+
+
+#if 0
 int fileReadData(ftpFileInfo_t* ftpFileInfo, char** fileBuffer)
 {
     FILE *fp = NULL;
@@ -135,7 +195,7 @@ int fileReadData(ftpFileInfo_t* ftpFileInfo, char** fileBuffer)
     strcat(fileName,ftpFileInfo->destFilePath);
     strcat(fileName,ftpFileInfo->destFileName);
 
-
+    
 /*
     if(fopen_s(&fp, fileName, "rb") != 0) 
     {
@@ -205,16 +265,8 @@ int fileReadData(ftpFileInfo_t* ftpFileInfo, char** fileBuffer)
 
 
 }
-
-
-
-int fileWriteData()
-{
-    return SUCCESS;
-}
-
+#endif
 
 void itoa(int value, char *str, int base) { static char num[]="0123456789abcdefghijklmnopqrstuvwxyz"; char *wstr=str; int sign; if(base<2 || base>35){ *wstr='\0'; return; } if((sign=value) <0) value=-value; do *wstr++=num[value%base]; while(value/=base); if(sign<0) *wstr='\0'; strreverse(str, wstr-1); }
 
 void strreverse(char *begin, char * end) { char aux; while(end>begin) aux=*end, *end--=*begin, *begin++=aux; }
-
--- c/libftp/libftp_example.h
+++ c/libftp/libftp_example.h
@@ -5,22 +5,55 @@
 #define SUCCESS 0
 #define ERROR -1
 
+#define GET_FTP_IP ""
+#define GET_FTP_LOGIN_ID ""
+#define GET_FTP_LOGIN_PSWD ""
+#define GET_FTP_SRC_FILEPATH ""
+#define GET_FTP_SRC_FILENAME ""
+#define GET_FTP_DEST_FILEPATH ""
+#define GET_FTP_DEST_FILENAME ""
+
+
+#define PUT_FTP_IP ""
+#define PUT_FTP_LOGIN_ID ""
+#define PUT_FTP_LOGIN_PSWD ""
+#define PUT_FTP_SRC_FILEPATH ""
+#define PUT_FTP_SRC_FILENAME ""
+#define PUT_FTP_DEST_FILEPATH ""
+#define PUT_FTP_DEST_FILENAME ""
 
 
 typedef struct ftpConnectInfo
 {
-    char ip[50+1];
-    char loginID[50+1];
-    char loginPassword[50+1];  
+    char getFtpIp[50+1];
+    char getFtpLoginId[50+1];
+    char getFtpLoginPassword[50+1];  
+
+    char putFtpIp[50+1];
+    char putFtpLoginId[50+1];
+    char putFtpLoginPassword[50+1];  
 }ftpConnectInfo_t;
 
 
 typedef struct ftpFileInfo
 {
-    char srcFilePath[50+1];
-    char srcFileName[50+1];
+    char getFtpSrcFilePath[50+1];
+    char getFtpSrcFileName[50+1];
+    char getFtpDestFilePath[50+1];
+    char getFtpDestFileName[50+1];
 
-    char destFilePath[50+1];
-    char destFileName[50+1];
+    char putFtpSrcFilePath[50+1];
+    char putFtpSrcFileName[50+1];
+    char putFtpDestFilePath[50+1];
+    char putFtpDestFileName[50+1];
+    
     int fileSize;
 }ftpFileInfo_t;
+
+
+int getConnectInfo(ftpConnectInfo_t* ftpConnectInfo);
+int getFtpFileInfo(ftpFileInfo_t* ftpFileInfo);
+int ftpFileGet(ftpConnectInfo_t* ftpConnectInfo, ftpFileInfo_t* ftpFileInfo);
+int ftpFilePut(ftpConnectInfo_t* ftpConnectInfo, ftpFileInfo_t* ftpFileInfo);
+void itoa(int value, char *str, int base);
+void strreverse(char *begin, char * end);
