98e4d47afcfbf3db3685873c2febc494a98a05bf
andamiro <dlfrb2002@gmail.com>
2018-12-21 20:58:32 +0900

    라즈베리파이 transmisson-daemon Dockerfile 작성

---
+++ Docker/rasp_transmission_daemon/AutoDelete.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+ID="pi";
+PASSWORD="";
+PORT="9091";
+
+SERVER="$PORT --auth $ID:$PASSWORD"
+TORRENTLIST=`transmission-remote $SERVER --list | sed -e '1d;$d;s/^ *//' | cut --only-delimited --delimiter=" " --fields=1`
+
+for TORRENTID in $TORRENTLIST
+do
+    DL_COMPLETED=`transmission-remote $SERVER --torrent $TORRENTID --info | grep "Percent Done: 100%"`
+    STATE_STOPPED=`transmission-remote $SERVER --torrent $TORRENTID --info | grep "State: Seeding\|Stopped\|Finished\|Idle"`
+    if [ "$DL_COMPLETED" ] && [ "$STATE_STOPPED" ]; then
+        transmission-remote $SERVER --torrent $TORRENTID --remove
+    fi
+done 
